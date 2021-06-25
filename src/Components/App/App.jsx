9a774eca8f4c9643cdb7a3580cda79d1726d1f80
GsSteven <65253485+GsSteven@users.noreply.github.com>
2021-06-25 17:27:26 -0400

    start of loader 3

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import CloseButton1 from "../CloseButton1/CloseButton1";
 import Loader1 from "../Loader1/Loader1";
 import Loader2 from "../Loader2/Loader2";
+import Loader3 from "../Loader3/Loader3";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <CloseButton1 />
       <Loader1 />
       <Loader2 />
+      <Loader3 />
     </div>
   );
 }
---
+++ src/Components/Loader3/Loader3.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { motion } from "framer-motion";
+import "./Loader3.css";
+
+export default function Loader3() {
+  return <div className="loader3"></div>;
+}
