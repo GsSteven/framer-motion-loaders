0f111fa0b9b4652b62b00aaa53ffc7b78e648395
GsSteven <65253485+GsSteven@users.noreply.github.com>
2021-06-25 17:23:25 -0400

    triple dot loader added

--- src/Components/Loader2/Loader2.jsx
+++ src/Components/Loader2/Loader2.jsx
@@ -5,7 +5,44 @@ import "./Loader2.css";
 export default function Loader2() {
   return (
     <div className="loader2">
-      <motion.div id="ball1" />
+      <motion.div
+        id="ball1"
+        animate={{
+          opacity: [1, 0, 1],
+          scale: [1, 0.75, 1],
+          transition: {
+            duration: 2,
+            ease: "easeInOut",
+            repeat: Infinity,
+          },
+        }}
+      />
+      <motion.div
+        id="ball2"
+        animate={{
+          opacity: [1, 0, 1],
+          scale: [1, 0.75, 1],
+          transition: {
+            duration: 2,
+            delay: 0.5,
+            ease: "easeInOut",
+            repeat: Infinity,
+          },
+        }}
+      />
+      <motion.div
+        id="ball3"
+        animate={{
+          opacity: [1, 0, 1],
+          scale: [1, 0.75, 1],
+          transition: {
+            duration: 2,
+            delay: 1,
+            ease: "easeInOut",
+            repeat: Infinity,
+          },
+        }}
+      />
     </div>
   );
 }
